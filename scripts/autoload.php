b13bdfc4a605e99d3968e61a65769f52fe7296b7
Salvador Barron <sonny@spaceshiplabs.com>
2011-11-25 18:08:04 -0600

    contact module updates

--- scripts/autoload.php
+++ scripts/autoload.php
@@ -3,18 +3,18 @@ function __autoload($class_name){
 	global $config_name;
 	$config = new $config_name();
 	$folders = array(
+		$config->document_root."/controlers/controler.",
 		$config->mxnphp_dir."/classes/",
-		$config->document_root."/models/model.",
-		$config->document_root."/controlers/controler."
+		$config->document_root."/models/model."
 	);
 	$i = 0;
 	$size = count($folders);
 	do{
 		$file = $folders[$i++].$class_name.".php";
+		//echo $file."<br/>";
 	}while(!file_exists($file) && $i<$size);
 	if(file_exists($file)){
 		include_once $file;
-	}//else
-		//echo "class $class_name not found";
+	}
 }
 ?>
